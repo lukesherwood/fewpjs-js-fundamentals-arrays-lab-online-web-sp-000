3ee4845cfb2c11da0d1bd61855fc3b34bf77c3ca
Luke Sherwood <luke.sherwood55@gmail.com>
2020-05-18 01:30:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution here!
-const pets = ["Milo", "Otis", "Garfield"]
-const append = pets.push('Odie')
+const pets = 
+const append = ["Milo", "Otis", "Garfield"].push('Odie')
 const prepend = pets.unshift('Odie')
 const removeLast = pets.pop()
 const removeFirst = pets.shift()
