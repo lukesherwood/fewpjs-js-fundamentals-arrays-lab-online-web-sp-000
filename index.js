1112ed9c17b50b27f60c1a9e6f4a8ecb9d30b4f2
Luke Sherwood <luke.sherwood55@gmail.com>
2020-05-18 01:30:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 const pets = ["Milo", "Otis", "Garfield"]
 const append = pets.push('Odie')
 const prepend = pets.unshift('Odie')
-const removeLast = pets.
+const removeLast = pets.pop()
+const removeFirst = pets.
